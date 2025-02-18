adb6fc55ee46d64963a3222b5fb54a95981c6a6c
MortiestMorty <d.arroyo14@student.keiseruniversity.edu>
2025-02-17 23:09:52 -0500

    fix

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,47 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+/*
+Write a Queue client that takes a command-line argument k and prints the kth from the last string
+found on standard input (assuming that standard input has k or more strings).
+*/
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
-}
+public class Main
+{
+    public static void main(String[] args)
+    {
+        if (args.length != 1)
+        {
+            System.out.println("Please provide k as a command-line argument.");
+            return;
+        }// if statement
+
+        int k = Integer.parseInt(args[0]);
+
+        Queue<String> queue = new LinkedList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext())
+        {
+            String word = scanner.next();
+            queue.add(word); // Add new word to the queue
+
+            if (queue.size() > k)
+            {
+                queue.poll();
+
+            }//emd if statement
+        }//end while loop
+        scanner.close();
+
+        if (queue.size() < k)
+        {
+            System.out.println("Not enough words in input.");
+        } // end if statement
+        else
+        {
+            System.out.println("The " + k + "-th last word is: " + queue.peek());
+        }// end else condition
+
+    }//end main
+}//end class
